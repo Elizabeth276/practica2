32ec72ebfbaf39d5033332918c3ff08fbeb3e270
Elizabeth <18031189@itcelaya.edu.mx>
2022-11-10 20:21:20 -0600

    [ADD] Clase 31-08-22

---
+++ lib/screens/login_screen.dart
@@ -0,0 +1,112 @@
+import 'package:flutter/material.dart';
+import 'package:social_login_buttons/social_login_buttons.dart';
+
+class LoginScreen extends StatefulWidget {
+  LoginScreen({Key? key}) : super(key: key);
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  final txtUser = TextField(
+    decoration: InputDecoration(
+      hintText: 'Introduce el usuario',
+      label: Text('Correo Electrónico'),
+    ),
+  );
+
+  final txtPwd = TextField(
+    obscureText: true,
+    decoration: InputDecoration(
+      hintText: 'Introduce el password',
+      label: Text('Contraseña'),
+    ),
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      body: Container(
+        padding: EdgeInsets.all(MediaQuery.of(context).size.width / 20),
+        height: MediaQuery.of(context).size.height,
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/fondo_login.png'),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: Stack(
+          alignment: Alignment.center,
+          children: [
+            Positioned(
+              top: MediaQuery.of(context).size.width / 5,
+              child: Image.asset(
+                'assets/mario_logo.png',
+                width: MediaQuery.of(context).size.width / 1.5,
+              ),
+            ),
+            Container(
+              padding: EdgeInsets.only(
+                left: MediaQuery.of(context).size.width / 20,
+                right: MediaQuery.of(context).size.width / 20,
+                bottom: MediaQuery.of(context).size.width / 20,
+              ),
+              color: Colors.white,
+              child: ListView(
+                shrinkWrap: true,
+                children: [
+                  txtUser,
+                  SizedBox(
+                    height: 15,
+                  ),
+                  txtPwd,
+                ],
+              ),
+            ),
+            Positioned(
+              bottom: MediaQuery.of(context).size.width / 2,
+              right: MediaQuery.of(context).size.width / 20,
+              child: GestureDetector(
+                onTap: () {
+                  print('hola');
+                },
+                child: Image.asset('assets/bloque.png',
+                    height: MediaQuery.of(context).size.width / 5),
+              ),
+            ),
+            Positioned(
+              bottom: MediaQuery.of(context).size.width / 50,
+              child: Container(
+                padding: EdgeInsets.symmetric(
+                    horizontal: MediaQuery.of(context).size.width / 20),
+                width: MediaQuery.of(context).size.width / 1.1,
+                child: Column(children: [
+                  SocialLoginButton(
+                    buttonType: SocialLoginButtonType.facebook,
+                    onPressed: () {},
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  SocialLoginButton(
+                    buttonType: SocialLoginButtonType.github,
+                    onPressed: () {},
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  SocialLoginButton(
+                    buttonType: SocialLoginButtonType.google,
+                    onPressed: () {},
+                  ),
+                ]),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
